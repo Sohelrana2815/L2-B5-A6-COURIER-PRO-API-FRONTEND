ab44f916dacdd967cf02be4b3081c44298c174fa
Sohelrana2815 <sohel152302@gmail.com>
2025-10-15 11:45:44 +0600

    successfully added: 1. shadcn 2. navbar,footer 3. dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Outlet } from "react-router";
 import CommonLayout from "./components/layouts/CommonLayout";
-import Navbar from "./components/layouts/Navbar";
-import Footer from "./components/layouts/Footer";
 
 function App() {
   return (
     <>
       <div>
         <CommonLayout>
-          <Navbar />
           <Outlet />
-          <Footer />
         </CommonLayout>
       </div>
     </>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,70 @@ export default function About() {
   return (
     <div>
       <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
+      <h1>This is About page</h1>
+      lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolorem.
     </div>
   );
 }
+  
